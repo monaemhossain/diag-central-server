0a85802d6dd20f1e3bb8eaddc3d66c4a33ad11b3
monaemhossain <monaemhs@gmail.com>
2023-11-28 17:35:04 +0300

    added jwt

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: ['https://diag-central.web.app', 'http://localhost:5173'],
+    credentials: true
+}))
 app.use(express.json());
+const verifyToken = async(req, res, next) => {
+    const token = req.cookies?.token
+    if(!token){
+        return res.status(401).send("unauthorized access")
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if(err) {
+            return res.status(401).send({message: 'unauthorized access'})
+        }
+        req.user = decoded;
+        next()        
+    })
+
+}
 
 
 
@@ -28,6 +46,34 @@ const client = new MongoClient(uri, {
 client.db("admin").command({ ping: 1 });
 console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
+// auth api
+app.post('/jwt', async (req, res) => {
+    const user = req.body;
+    console.log(user);
+    const token = jwt.sign(user, `${process.env.ACCESS_TOKEN_SECRET}`, { expiresIn: '1h' })
+    res
+        .cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none',
+            maxAge: 60 * 60 * 1000
+        })
+        .send({ success: true })
+})
+// jwt logout
+app.post('/logout', async (req, res) => {
+    const user = req.body;
+    console.log("requested");
+    res.clearCookie('token', { maxAge: 0, secure: true, sameSite: 'none' }).send('logged out successfully')
+})
+
+
+
+
+
+
+
+
 const userCollection = client.db('usersDB').collection('users');
 // Create user
 app.post('/users', async (req, res) => {
@@ -42,11 +88,37 @@ app.get('/users', async (req, res) => {
     const result = await cursor.toArray();
     res.send(result);
 })
+// read single user
+app.get('/user/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await userCollection.findOne(query);
+    res.send(result);
+})
+// update user
+app.put('/user/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) }
+    const options = { upsert: true }
+    const updateUser = req.body;
+
+    const user = {
+        $set: {
+            role: updateUser.role,
+            activeStatus: updateUser.activeStatus,
+            userName: updateUser.userName,
+            userBloodGroup: updateUser.userBloodGroup,
+        }
+    }
+    const result = await userCollection.updateOne(filter, user, options);
+    res.send(result);
+})
+
+
+
+
+
 const testCollection = client.db('testsDB').collection('tests');
-
-
-
-
 
 
 
